a943127f14f7afa67ca07713ce455c1608d0963f
mikaijun <mj.tempe443@gmail.com>
2024-02-22 18:29:03 +0900

    並び順の修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,8 @@
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended', // (A)
-    'plugin:@typescript-eslint/recommended-requiring-type-checking', // (B)
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'next/core-web-vitals',
     'prettier',
   ],
@@ -14,6 +14,61 @@ module.exports = {
   parserOptions: {
     project: './tsconfig.json',
   },
-  plugins: ['@typescript-eslint'],
+  plugins: ['@typescript-eslint', 'import'],
   root: true,
+  // NOTE: ルールに関しては以下の記事を参考
+  // see: https://zenn.dev/resistance_gowy/articles/91b4f62b9f48ec#eslint-plugin-import
+  rules: {
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          ['parent', 'sibling'],
+          'object',
+          'type',
+          'index',
+        ],
+        'newlines-between': 'always',
+        pathGroupsExcludedImportTypes: ['builtin'],
+        pathGroups: [
+          {
+            pattern: '@/utils/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/libs/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/hooks/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/components/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/const/**',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '@/types/**',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+  },
 }
