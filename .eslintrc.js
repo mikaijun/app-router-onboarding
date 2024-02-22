b25d497dca2b6f22853df38051532a440b03e44e
mikaijun <mj.tempe443@gmail.com>
2024-02-22 18:05:45 +0900

    typescript-eslintの導入

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,19 @@
+// NOTE: ESLintのルールを設定
+// see: https://zenn.dev/resistance_gowy/articles/91b4f62b9f48ec#typescript-eslint
+
 /** @type {import('eslint').ESLint.ConfigData} */
 module.exports = {
-  extends: ['next/core-web-vitals', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended', // (A)
+    'plugin:@typescript-eslint/recommended-requiring-type-checking', // (B)
+    'next/core-web-vitals',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+  },
+  plugins: ['@typescript-eslint'],
+  root: true,
 }
