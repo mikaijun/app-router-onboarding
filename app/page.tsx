9724d9e035820169a1a884bb961674ac4011ab2e
mikaijun <mj.tempe443@gmail.com>
2024-02-26 14:25:56 +0900

    chakra ui 導入

--- app/page.tsx
+++ app/page.tsx
@@ -1,74 +1,31 @@
-import Image from 'next/image'
+import { Card, CardBody, Tabs, TabList, Tab, TabPanels, TabPanel } from '@chakra-ui/react'
 
 export default function Home() {
   return (
-    <main>
-      <div>
-        <p>
-          Get started by editingあいうえお&nbsp;
-          <code>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            By
-            <Image src='/vercel.svg' alt='Vercel Logo' width={100} height={24} priority />
-          </a>
-        </div>
-      </div>
-
-      <div>
-        <Image src='/next.svg' alt='Next.js Logo' width={180} height={37} priority />
-      </div>
-
-      <div>
-        <a
-          href='https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href='https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>Instantly deploy your Next.js site to a shareable URL with Vercel.</p>
-        </a>
-      </div>
+    <main className='flex flex-col items-center justify-between p-24'>
+      <Card className='mt-4'>
+        <CardBody>
+          <h2>Tabs</h2>
+          <Tabs>
+            <TabList>
+              <Tab>tab1</Tab>
+              <Tab>tab2</Tab>
+              <Tab>tab3</Tab>
+            </TabList>
+            <TabPanels>
+              <TabPanel>
+                <p>content1</p>
+              </TabPanel>
+              <TabPanel>
+                <p>content2</p>
+              </TabPanel>
+              <TabPanel>
+                <p>content3</p>
+              </TabPanel>
+            </TabPanels>
+          </Tabs>
+        </CardBody>
+      </Card>
     </main>
   )
 }
